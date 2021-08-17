c0fff23c8348a67d23263c94174c0ac1c355c362
Amit Pandey <amitpnd018@gmail.com>
2021-08-17 05:52:46 +0530

    Controller, Routes and layout setup

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express=require('express');
 const port=8080;
-
+const expressLayouts=require('express-ejs-layouts');
+const mongoose=require('mongoose');
 const app=express();
+const passport=require('passport');
+const sassMiddleware=require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+app.use(express.urlencoded());
+
+//Express Layouts Setup
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+// View engine setup 
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//use express Router
+app.use('/',require('./routes'));
 
 app.listen(port,function(err){
     if(err){
-        console.log('Cannot run Server !!!');
+        console.log('Cannot run Server !!!',err);
     }
     console.log('Server Running on port - ',port);
 });
